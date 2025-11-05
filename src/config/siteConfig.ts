028c544f0d4bfa7039597e9a3eaa7bc9b7c18e13
Parssama2233 <hello.windholm@gmail.com>
2025-11-05 15:03:23 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -44,7 +44,7 @@ export const siteConfig: SiteConfig = {
   // { type: "image", value: "https://example.com/logo.png", alt: "Firefly Logo" }
   navbarLogo: {
     type: "image",
-    value: "/assets/images/LiuYingPure3.svg",
+    value: "/assets/images/IMG_20251105_144406.jpg",
     alt: "🍀",
   },
   navbarTitle: "WindHolm", // 导航栏标题，可以设置为与 title 不同的值，如果不设置则使用 title
